b2d73b299f3ebafcdde1a619ef62eb35ef6f368e
Phil Brownlie <philbrownlie@outlook.com>
2023-08-29 19:38:55 +0100

    Created in VS Code - commit to Git

---
+++ Main.go
@@ -0,0 +1,17 @@
+// Note: "package" declaration below
+package main
+
+// Note: "import" block below
+import (
+	"fmt"
+)
+
+// Note: "var" block below
+var (
+	i int = 10
+)
+
+func main() {
+	fmt.Println("Hello to the interesting world of Golang")
+	fmt.Println(i)
+}
